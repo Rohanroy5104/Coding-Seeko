7a99bccfcd38110e6466fd1234a6771d10769c9a
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-26 11:33:12 +0000

    Write a C# program to create an new array, cointaining the middle elements of three arrays of integers.

---
+++ NewArray/NewArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 4, 5 };
+            Console.WriteLine("\nArray1 : [{0}]", string.Join(", ", array1));
+            int[] array2 = { 2, 4, 9 };
+            Console.WriteLine("\nArray2 : [{0}]", string.Join(", ", array2));
+            int[] array3 = { 3, 3, 7 };
+            Console.WriteLine("\nArray3 : [{0}]", string.Join(", ", array3));
+            int[] new_array = { array1[1], array2[1], array3[1] };
+            Console.WriteLine("\nNew array : [{0}]", string.Join(", ", new_array));
+            Console.ReadLine();
+
+
+        }
+    }
+}

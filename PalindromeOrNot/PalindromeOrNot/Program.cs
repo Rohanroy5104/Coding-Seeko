5a2313a1d327f56e1c8bee35938b8e3ddf6568f5
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-26 12:47:44 +0000

    Checking Palindrome or not.

---
+++ PalindromeOrNot/PalindromeOrNot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeOrNot
+{
+    internal class Program
+    {
+        public static bool checkPalindrome(string inputString)
+        {
+            char[] c = inputString.ToCharArray();
+            Array.Reverse(c);
+            return new string(c).Equals(inputString);
+        }
+        public static void Main()
+        {
+            Console.WriteLine(checkPalindrome("aaa"));
+            Console.WriteLine(checkPalindrome("abc"));
+            Console.WriteLine(checkPalindrome("madam"));
+            Console.WriteLine(checkPalindrome("121121"));
+            Console.ReadLine();
+        }
+
+
+
+
+
+        }
+    }
+

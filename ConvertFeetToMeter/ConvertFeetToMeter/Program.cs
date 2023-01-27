0a81f89465cc38356ecd7cef6c5aa1fc9447df03
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-27 07:55:10 +0000

    Converting feet to meter.

---
+++ ConvertFeetToMeter/ConvertFeetToMeter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertFeetToMeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meter, feet;
+            Console.WriteLine("Input the number in feet : ");
+            feet = Convert.ToInt32(Console.ReadLine());
+            meter = feet / 3.2808399;
+            Console.WriteLine("\nFeet in meter : " + meter);
+            Console.ReadLine();
+        }
+    }
+}

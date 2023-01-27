d735c69c5c3e796404b15a8f4372357e191e1275
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-27 07:14:16 +0000

    Multipling floating point variables.

---
+++ MultiplyFloatingPointNumber/MultiplyFloatingPointNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyFloatingPointNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, x;
+            Console.WriteLine("Input the first number : ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input the second number : ");
+            b = Convert.ToDouble(Console.ReadLine());
+            x = a * b;
+            Console.WriteLine("Result is : " + x);
+            Console.ReadLine();
+
+        }
+    }
+}

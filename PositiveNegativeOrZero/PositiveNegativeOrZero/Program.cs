22c6bf5a39768dcc8efd8d347a87b23a432f76a4
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-28 13:23:59 +0000

    Checking the number is positive or negative.

---
+++ PositiveNegativeOrZero/PositiveNegativeOrZero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveNegativeOrZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter any number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("Entered number is positive");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("Entered number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is zero");
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}

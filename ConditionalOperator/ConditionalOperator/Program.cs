d5233c75840b4deda7082e45feb85b7fb1029e84
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-28 11:38:57 +0000

    Finding the largest number using conditional operator.

---
+++ ConditionalOperator/ConditionalOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, Largest;
+            Console.WriteLine("Enter the first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Largest = num1 > num2 ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+            Console.WriteLine(Largest + " is the Largest number");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}

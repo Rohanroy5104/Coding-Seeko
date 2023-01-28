7863e0732ee86963cdb679a8a461a443a67e81fd
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-28 12:48:11 +0000

    Check whether the alphabet is vowel or not.

---
+++ CheckVowelOrNot/CheckVowelOrNot/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckVowelOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.Write("Input an Alphabet A - Z or a - z : ");
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+            int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("you entered a number, Please enter an alphabet.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("Alphabet is not vowel");
+                        break;
+                }
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}

4dc09747a220a2d7b6476aaed9bddec3a889a44d
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-28 12:06:43 +0000

    Check whether the entered number is a leap year or not.

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter Year");
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(year % 4 == 0 ? "Entered year is a Leap year" : "Not leap year");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}

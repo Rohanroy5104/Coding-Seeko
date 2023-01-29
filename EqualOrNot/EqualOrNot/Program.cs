87b42f6367218aa7b88690cc7c068b8d796f2216
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-29 12:33:01 +0000

    equal or not.

---
+++ EqualOrNot/EqualOrNot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Input the value for Number1 and Number2 : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Number1 and Number2 are equal\n");
+            }
+            else
+            {
+                Console.WriteLine("Number1 and Number2 are not equal \n");
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

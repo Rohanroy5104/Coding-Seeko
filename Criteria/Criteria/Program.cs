271c276b64cafe924e2a6dd1e0807938cb50438f
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-29 17:05:16 +0000

    Check the criteria to get admission in engineering college.

---
+++ Criteria/Criteria/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criteria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks;
+            Console.WriteLine("Enter the average marks of PCM : ");
+            marks = Convert.ToInt32(Console.ReadLine());
+            if (marks > 63)
+            {
+                Console.WriteLine("Entered marks is eligible for an engineering course");
+            }
+            if (marks < 63)
+            {
+                Console.WriteLine("Entered marks is not eligible for an engineering course");
+            }
+                
+                Console.ReadLine();
+
+
+
+
+
+
+            }
+        }
+    }
+
+

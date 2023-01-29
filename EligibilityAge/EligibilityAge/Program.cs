94ff89e8feca31f46bbf474f5c3509c62457cbbd
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-29 16:14:13 +0000

    To check whether the candidate is eligible or not...?

---
+++ EligibilityAge/EligibilityAge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter the age : ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+            {
+                Console.WriteLine("The cadidate is not eligible to caste vote");
+            }
+            else
+            {
+                Console.WriteLine("The candidate is eligible to caste vote");
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}

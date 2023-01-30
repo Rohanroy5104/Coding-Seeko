001bfa99bf68edd4d99b42c9693cebf2e9961b33
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-30 19:05:03 +0000

    Multiplication tables.

---
+++ Tables/Tables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+            Console.WriteLine("Enter the number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("{0} * {1} = {2}\n", num, i, num * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}

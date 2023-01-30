d0fbceb38dc5fe89a04f29c4fc5ab2478f32800d
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-30 18:49:45 +0000

    Alphabets from a to z.

---
+++ Alphabets/Alphabets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Alphabets from a - z are: ");
+            for (ch = 'a'; ch <= 'z'; ch++)
+            {
+                Console.Write(ch + "\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}

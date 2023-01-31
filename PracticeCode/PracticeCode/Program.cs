0c5cf643dafa061f504225c6c58d9d236d2cfb8e
rohanroy8427@gmail.com <rohanroy8427@gmail.com>
2023-01-31 18:39:57 +0000

    Sum of odd numbers.

---
+++ PracticeCode/PracticeCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num, sum = 0;
+            Console.WriteLine("Enter any number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(i = 1; i <= num; i += 2)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of all odd number from 1 to " + num + " = " + sum);
+            Console.ReadLine();
+      
+        }
+    }
+}
